5e974b7e563b742cd09761bb5c4abf8dddcae4aa
mamapuro55 <mamapuro55@gmail>
2026-06-27 17:26:18 +0900

    修正１

--- js/main.js
+++ js/main.js
@@ -84,6 +84,9 @@ $(function () {
 
     $("#modal-overlay").addClass("active");
 
+    // TOPボタンを非表示
+    $("#js-pagetop").removeClass("is-show");
+
     // 背景スクロール停止
     $("body").css("overflow", "hidden");
   });
@@ -111,3 +114,61 @@ $(function () {
   }
 
 });
+
+$(function () {
+
+  function fadeInAnimation() {
+
+    $('.fade-in').each(function () {
+
+      const target = $(this).offset().top;
+      const scroll = $(window).scrollTop();
+      const windowHeight = $(window).height();
+
+      if (scroll > target - windowHeight + 100) {
+        $(this).addClass('is-show');
+      }
+
+    });
+
+  }
+
+  fadeInAnimation();
+
+  $(window).on('scroll', function () {
+    fadeInAnimation();
+  });
+
+});
+
+
+// ------------------------------------------
+// 1. スクロール位置でヘッダー・TOPボタン切り替え
+// ------------------------------------------
+const $pageTop = $('#js-pagetop');
+
+function checkScroll() {
+  const scrollY = $(window).scrollTop();
+  const aboutTop = $('#about').length ? $('#about').offset().top : Infinity;
+
+  const isScrolled = scrollY >= aboutTop;
+
+  // ヘッダー切り替え
+  $header.toggleClass('is-scrolled', isScrolled);
+
+  // TOPボタン表示
+  $pageTop.toggleClass('is-show', isScrolled);
+}
+
+$(function () {
+  const $pageTop = $('#js-pagetop');
+
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > 300) {
+      $pageTop.addClass('is-show');
+    } else {
+      $pageTop.removeClass('is-show');
+    }
+  });
+});
+
